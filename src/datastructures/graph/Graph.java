2aa2558b28f6645371369c6083fbd53bec562b24
Vijay Anand Shanmughadass <vijayanandinbox@gmail.com>
2017-07-04 12:14:02 +0530

    Added Breadth First Search implementation

--- src/datastructures/graph/Graph.java
+++ src/datastructures/graph/Graph.java
@@ -47,24 +47,48 @@ public class Graph {
 		Stack stack = new Stack(getVertexCount());
 		stack.push(0);// Pushed A into Stack
 		vertexList[0].wasVisited = true;
-		System.out.print(vertexList[0].label);
-		getAdjacentUnvisitedNode(stack);
+		System.out.print("Visits : " + vertexList[0].label);				
+		while (stack.isNotEmpty()){
+			int adjVertex = getAdjacentUnvisitedVertex(stack.peek());
+			if(adjVertex == -1){
+				stack.pop();
+			}
+			else {
+				stack.push(adjVertex);
+				vertexList[adjVertex].wasVisited = true;
+				System.out.print(vertexList[adjVertex].label);				
+			}			 			
+		}
 	}
-
-	void getAdjacentUnvisitedNode(Stack stack) {
-		int currentIndex = stack.peek();
-		for (int col = 0; col < vertexCount; col++) {
-			if (adjacencyMatrix[currentIndex][col] == 1 && !vertexList[col].wasVisited) {
-				stack.push(col);
-				vertexList[col].wasVisited = true;
-				System.out.print(vertexList[col].label);
-				getAdjacentUnvisitedNode(stack);
+	
+	void performBFS() {
+		Queue queue = new Queue(getVertexCount());
+		queue.insert(0);// Pushed A into Queue
+		vertexList[0].wasVisited = true;
+		System.out.print("Visits : " + vertexList[0].label);
+		int currentVertex = queue.remove();
+		while (currentVertex != -1) {
+			int adjVertex = getAdjacentUnvisitedVertex(currentVertex);
+			if (adjVertex != -1) {
+				queue.insert(adjVertex);
+				vertexList[adjVertex].wasVisited = true;
+				System.out.print(vertexList[adjVertex].label);
+			} else {
+				currentVertex = queue.remove();
 			}
 		}
-		if (stack.pop() != -1) {
-			return;
-		}
 	}
+
+	int getAdjacentUnvisitedVertex(int currentVertex) {
+		for (int col = 0; col < vertexCount; col++) {
+			if (adjacencyMatrix[currentVertex][col] == 1 && !vertexList[col].wasVisited) {
+				return col;
+			}
+		}
+		return -1;
+	}
+
+
 
 }
 
@@ -98,7 +122,7 @@ class Stack {
 
 	protected int pop() {
 		if (pointer < 0) {
-			System.out.println("Stack Empty. Can't Pop.");
+			System.out.println("\nStack Empty. Can't Pop.");
 			return -1;
 		}
 		return array[pointer--];
@@ -110,5 +134,52 @@ class Stack {
 
 	protected boolean isNotEmpty() {
 		return pointer > -1;
+	}
+}
+
+class Queue {
+
+	int[] array = null;
+	int queueSize = 0;
+	int front, rear;
+	int numberOfItems = 0;
+
+	Queue(int size) {
+		array = new int[size];
+		queueSize = size;
+		front = 0;
+		rear = -1;
+	}
+
+	void insert(int index) {
+		if (numberOfItems >= queueSize) {
+			System.out.println("Queue Full. Cannot Insert");
+			return;
+		}
+		if ((rear + 1) >= queueSize) {
+			rear = 0;
+		}
+		array[++rear] = index;
+		numberOfItems++;
+	}
+
+	int remove() {
+		if (numberOfItems <= 0) {
+			System.out.println("\nQueue Empty. Cannot Remove");
+			return -1;
+		}
+		if ((front + 1) >= queueSize) {
+			front = 0;
+		}
+		numberOfItems--;
+		return array[front++];
+	}
+
+	private int frontPeek() {
+		return array[front];
+	}
+
+	private int rearPeek() {
+		return array[rear];
 	}
 }
